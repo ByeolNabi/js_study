81d1bd5f3f00d0fe97b3dffbeece62b7497e1916
ByeolNabi <20183338@office.deu.ac.kr>
2024-01-10 21:59:22 +0900

    function 실습

---
+++ hello_js/function.js
@@ -0,0 +1,14 @@
+function sum(a, b){
+    return a+b;s
+}
+
+function print(str){
+    console.log(str);
+    return 0;
+    
+    return 3; // 실행되지 않음
+
+}
+
+print('hello world');
+print(String(sum(3,5)));
