c44f858995840ea59a1b31a6910fd6853728f71f
ByeolNabi <20183338@office.deu.ac.kr>
2024-01-10 20:13:10 +0900

    lecture04 실습
    -데이터타입 설명

---
+++ hello_js/lecture04.js
@@ -0,0 +1,9 @@
+var a1 = 1435;
+var a2 = 234.534;
+var b1 = "안녕하다";
+var c1 = true;
+
+console.log(a1, typeof(a1));
+console.log(a2, typeof(a2));
+console.log(b1, typeof(b1));
+console.log(c1, typeof(c1));
